d3b28e1b1cd07bfc40811f236d395acc5c6b41c6
grammlaman <grammlaman@gmail.com>
2019-12-03 17:05:55 +0600

    Работаем над мобилкой

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,1 +1,5 @@
-
+let pageSelect = document.querySelector('.page-select');
+pageSelect.onclick = function() {
+    if(pageSelect.classList.length==2){pageSelect.classList.add('open');return}
+    if(pageSelect.classList.length!==2){pageSelect.classList.remove('open');}};
+let fixed = document.querySelector('.mob-fixed');
