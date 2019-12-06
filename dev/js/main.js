4e909243028c316329f207438c018177087fa0eb
grammlaman <grammlaman@gmail.com>
2019-12-06 16:59:51 +0600

    Пару блоков почти готовы

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,5 +1,28 @@
 let pageSelect = document.querySelector('.page-select');
 pageSelect.onclick = function() {
-    if(pageSelect.classList.length==2){pageSelect.classList.add('open');return}
-    if(pageSelect.classList.length!==2){pageSelect.classList.remove('open');}};
+    if(!pageSelect.classList.contains('open')){pageSelect.classList.add('open');return}
+    if(pageSelect.classList.contains('open')){pageSelect.classList.remove('open');}};
 let fixed = document.querySelector('.mob-fixed');
+
+window.onload = function () {
+    setInterval('changeBGC()',10000);
+};
+function changeBGC() {
+    let arr = document.querySelectorAll('.typycal');
+    arr.forEach(function (el) {
+        let cls = el.getAttribute('data-class'),
+            clsBgc = el.getAttribute('data-change');
+        if(el.classList.contains(clsBgc)){
+            el.classList.add(cls);
+            el.classList.remove(clsBgc);
+        }
+        else{
+            el.classList.add(clsBgc);
+            el.classList.remove(cls)
+
+        }
+    })
+
+
+
+}
